c0eabc0cb204f88fc8701e45747ba3d835ae9340
donuthole20 <donuthole20@gmail.com>
2021-05-18 18:45:29 +0800

    Intial code

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+template<typename t>
+void log(t text)
+{
+    std::cout<<text<<std::endl;
+}
+
+int main(int argc, char** argv)
+{
+    if(argc < 2)
+    {
+        log("Arguments too few");
+        return -1;
+    }
+    std::ifstream gitIgnoreFileStream{argv[1]};
+    if(!gitIgnoreFileStream)
+    {
+        log("Git ignore file doesnt exist");
+        return -1;
+    }
+
+    std::string entry;
+    while(std::getline(gitIgnoreFileStream, entry))
+    {
+        size_t entrySize = entry.size();
+        if(entrySize <= 0)
+        {
+            continue;
+        }
+
+        if(entry.at(0)=='#')
+        {
+            continue;
+        }
+
+        if(entry.at(entrySize-1) == '*' && entry.at(entrySize-2) == '/')
+        {
+            entry = entry.substr(0,entrySize-2);
+        }
+
+        std::string command = "del /S /Q /F \"" + std::string(argv[2]) + "\\" + entry + "\"";
+        system(command.c_str());
+    }
+    
+    return 0;
+}
